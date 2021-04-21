4e2c9863d3e63dcf630aac1ee366b470abfeede5
Jarrett Parker <jarrettparker34@gmail.com>
2021-04-20 22:14:00 -0400

    Added first program from Chapter 3

---
+++ Chapter3/whileLoop.c
@@ -0,0 +1,12 @@
+// Demonstrates how to properly use a while loop
+#include "stdio.h"
+
+int main () {
+    int i = 0;
+
+    while ( i < 11 ) {
+        printf("%d ", i);
+        i++;
+    }
+    printf("\n");
+}
